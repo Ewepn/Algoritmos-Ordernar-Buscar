092e1996040d7e36e2088e69d1e8c494749abe2c
Ewerton Nascimento <epnascimento.dev@gmail.com>
2022-04-03 12:11:20 -0300

    mergeSort

---
+++ mergeSort.js
@@ -0,0 +1,36 @@
+const priceSilverSwords = require('./silver-swords-list');
+
+const mergeSortSwords = (listSwords) => {
+
+    if(listSwords.length > 1) {
+        const midle = Math.floor(listSwords.length / 2);
+        const part1 = mergeSortSwords(listSwords.slice(0, midle));
+        const part2 = mergeSortSwords(listSwords.slice(midle, listSwords.length));
+        listSwords = orderSword(part1, part2);
+    }
+
+    return listSwords;
+};
+
+const orderSword = (receivePart1, receivePart2) => {
+    let positionPart1 = 0;
+    let positionPart2 = 0;
+    const result = [];
+    
+    while(positionPart1 < receivePart1.length && positionPart2 < receivePart2.length) {
+        let currentSword1 = receivePart1[positionPart1];
+        let currentSword2 = receivePart2[positionPart2];
+
+        if (currentSword1.price < currentSword2.price) {
+            result.push(currentSword1);
+            positionPart1++
+        } else {
+            result.push(currentSword2);
+            positionPart2++
+        }
+    }
+
+    return result.concat(positionPart1 < receivePart1.length ? receivePart1.slice(positionPart1) : receivePart2.slice(positionPart2));
+};
+
+console.log(mergeSortSwords(priceSilverSwords));
---
+++ silver-swords-list.js
@@ -0,0 +1,124 @@
+const witcherSilverSwords = [
+    {
+        nome: 'Legendary Manticore silver sword',
+        tier: 'Witcher Item',
+        weight: '2.06 kg',
+        price: 1070 
+    },
+    {
+        nome: 'Manticore silver sword',
+        tier: 'Witcher Item',
+        weight: '2.06 kg',
+        price: 879 
+    },
+    {
+        nome: 'Grandmaster Wolven silver sword',
+        tier: 'Witcher Item',
+        weight: '2.23 kg',
+        price: 1243 
+    },
+    {
+        nome: 'Grandmaster Ursine silver sword',
+        tier: 'Witcher Item',
+        weight: '2.36 kg',
+        price: 1070 
+    },
+    {
+        nome: 'Grandmaster Griffin silver sword',
+        tier: 'Witcher Item',
+        weight: '2.36 kg',
+        price: 1070 
+    },
+    {
+        nome: 'Grandmaster Feline silver sword',
+        tier: 'Witcher Item',
+        weight: '2.36 kg',
+        price: 1275 
+    },
+    {
+        nome: 'Ursine silver sword - mastercrafted',
+        tier: 'Witcher Item',
+        weight: '3.01 kg',
+        price: 1371 
+    },
+    {
+        nome: 'Ursine silver sword - superior',
+        tier: 'Witcher Item',
+        weight: '3.01 kg',
+        price: 1230 
+    },
+    {
+        nome: 'Ursine silver sword - enhanced',
+        tier: 'Witcher Item',
+        weight: '3.01 kg',
+        price: 1088 
+    },
+    {
+        nome: 'Ursine silver sword',
+        tier: 'Witcher Item',
+        weight: '2.29 kg',
+        price: 457
+    },
+    {
+        nome: 'Griffin silver sword - mastercrafted	',
+        tier: 'Witcher Item',
+        weight: '2.81 kg',
+        price: 1323
+    },
+    {
+        nome: 'Griffin silver sword - superior',
+        tier: 'Witcher Item',
+        weight: '2.81 kg',
+        price: 1108
+    },
+    {
+        nome: 'Griffin silver sword - enhanced',
+        tier: 'Witcher Item',
+        weight: '1.61 kg',
+        price: 473
+    },
+    {
+        nome: 'Griffin silver sword',
+        tier: 'Witcher Item',
+        weight: '1.7 kg',
+        price: 360
+    },
+    {
+        nome: 'Feline silver sword - mastercrafted',
+        tier: 'Witcher Item',
+        weight: '3.46 kg',
+        price: 1554
+    },
+    {
+        nome: 'Feline silver sword - superior',
+        tier: 'Witcher Item',
+        weight: '2.73 kg',
+        price: 1254
+    },
+    {
+        nome: 'Feline silver sword - enhanced',
+        tier: 'Witcher Item',
+        weight: '1.58 kg',
+        price: 680
+    },
+    {
+        nome: 'Feline silver sword',
+        tier: 'Witcher Item',
+        weight: '1.18 kg',
+        price: 440
+    },
+    {
+        nome: 'Viper silver sword',
+        tier: 'Witcher Item',
+        weight: '1.21 kg',
+        price: 312
+    },
+    {
+        nome: 'Viper venomous silver sword',
+        tier: 'Witcher Item',
+        weight: '2.98 kg',
+        price: 1039
+    }
+];
+
+module.exports = witcherSilverSwords;
