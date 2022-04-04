9db459da442482b77079b82ffa8e2cbe46efcc73
Ewerton Nascimento <epnascimento.dev@gmail.com>
2022-04-04 13:27:07 -0300

    Algoritmo quickSort

---
+++ quickSort.js
@@ -0,0 +1,51 @@
+const priceSilverSwords = require('./silver-swords-list');
+
+const quickSortSwords = (swords, left, right) => {
+    if(swords.length > 1){
+        let currentIndex = partition(swords, left, right);
+
+        if (left < currentIndex - 1) {
+            quickSortSwords(swords, left, currentIndex - 1);
+        }
+
+        if (currentIndex < right) {
+            quickSortSwords(swords, currentIndex, right);
+        }
+    }
+
+    return swords;
+}
+
+const partition = (swords, left, right) => {
+    let pivot = swords[Math.floor((left + right) / 2)];
+    let currentLeft = left;
+    let currentRight = right;
+
+    while (currentLeft <= currentRight) {
+        while (swords[currentLeft].price < pivot.price) {
+            currentLeft++;
+        }
+
+        while (swords[currentRight].price > pivot.price) {
+            currentRight--;
+        }
+
+        if (currentLeft <= currentRight) {
+           changePosition(swords, currentLeft, currentRight);
+           currentLeft++;
+           currentRight--; 
+        }
+    }
+
+    return currentLeft;
+}
+
+const changePosition = (swords, from, to) => {
+    const element1 = swords[from];
+    const element2 = swords[to];
+
+    swords[from] = element2;
+    swords[to] = element1;
+}
+
+console.log(quickSortSwords(priceSilverSwords, 0, priceSilverSwords.length - 1));
